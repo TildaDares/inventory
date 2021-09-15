e714b630c46008e55fee8d7ca9ba533f6903e2fc
TildaDares <mathildaudufo@gmail.com>
2021-09-15 09:41:16 +0100

    get all items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,9 +1,14 @@
 const Item = require("../models/item");
 
-exports.index = function(req, res, next) {
-  Item.find({}).populate('category').populate('brand').exec(function(err, results) {
-    if(err) { return next(err); }
+exports.index = function (req, res, next) {
+  Item.find({})
+    .populate("category")
+    .populate("brand")
+    .exec(function (err, results) {
+      if (err) {
+        return next(err);
+      }
 
-    res.render("index", { title: "All items", items: results });
-  })
-}
+      res.render("index", { title: "All items", items: results });
+    });
+};
