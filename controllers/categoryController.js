7e6a71c483615e99f399d7f74f9e427f902b0e13
TildaDares <mathildaudufo@gmail.com>
2021-09-16 16:39:48 +0100

    category create form

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,6 @@
 const Category = require("../models/category");
 const Item = require("../models/item");
+const { body, validationResult } = require("express-validator");
 
 exports.category_list = function (req, res, next) {
   Category.find({}).exec(function (err, results) {
@@ -21,3 +22,31 @@ exports.category_items = function (req, res, next) {
       res.render("category_items", { title: req.params.name, items: results });
     });
 };
+
+exports.category_create_get = function (req, res, next) {
+  res.render("category_brand_form", { title: "New Category" });
+};
+
+exports.category_create_post = [
+  body("name", "Name should not be empty").trim().isLength({ min: 1 }).escape(),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const category = new Category({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      res.render("category_brand_form", {
+        title: "New Category",
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      category.save(function (err) {
+        if (err) return next(err);
+
+        res.redirect("/categories");
+      });
+    }
+  },
+];
--- routes/inventory.js
+++ routes/inventory.js
@@ -11,8 +11,12 @@ router.get("/items/:name/:id", itemsController.index);
 
 router.get("/brands", brandsController.brand_list);
 router.get("/brand/:name/:id", brandsController.brand_items);
+router.get("/brands/create", brandsController.brand_create_get);
+router.post("/brands/create", brandsController.brand_create_post);
 
 router.get("/categories", categoryController.category_list);
 router.get("/category/:name/:id", categoryController.category_items);
+router.get("/category/create", categoryController.category_create_get);
+router.post("/category/create", categoryController.category_create_post);
 
 module.exports = router;
